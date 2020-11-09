36c523bb2abab3fb2442fa21b992e270406c5db0
Angi Daniela Burbano <angiburba@gmail.com>
2020-11-08 19:33:18 -0500

    Creacion componente2

--- CitasMedicas/src/app/app.module.ts
+++ CitasMedicas/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Componente1Component } from './components/componente1/componente1.component';
+import { Componente2Component } from './components/componente2/componente2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Componente1Component
+    Componente1Component,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
